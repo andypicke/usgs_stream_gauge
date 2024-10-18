638dbbd9726322aaa61e48d5c499bc81983e67cb
Andy <andypicke@gmail.com>
2024-10-18 09:21:14 -0600

    use plotly for timeseries plot

--- app.R
+++ app.R
@@ -58,9 +58,14 @@ ui <- page_sidebar(
     ),
     
     # timeseries plot from one station
-    nav_panel(title = "Timeseries Plot", 
-              plotOutput("ts_plot")
-    ),
+    # nav_panel(title = "Timeseries Plot", 
+    #           plotOutput("ts_plot")
+    # ),
+    
+    # Plotly plot
+    nav_panel(title = "Timeseries Plot",
+              plotlyOutput("ts_plot")
+              ),
     
     # Data table
     nav_panel(title = "Site Info Table", 
@@ -164,18 +169,33 @@ server <- function(input, output) {
   },server = FALSE)
   
   
-  output$ts_plot <- renderPlot({
-    
+  # make plot of timeseries for one stations : ggplot
+  # output$ts_plot <- renderPlot({
+  #   
+  #   parameterInfo <- attr(site_data(), "variableInfo")
+  #   siteInfo <- attr(site_data(), "siteInfo")
+  #   
+  #   site_data() |>
+  #     ggplot(aes(x = dateTime, y = Flow_Inst)) +
+  #     geom_line() +
+  #     ylab(parameterInfo$variableDescription) +
+  #     ggtitle(siteInfo$station_nm)
+  # })
+  
+  
+  # make plot of timeseries for one stations : plotly
+  output$ts_plot <- plotly::renderPlotly({
     parameterInfo <- attr(site_data(), "variableInfo")
     siteInfo <- attr(site_data(), "siteInfo")
     
-    
     site_data() |>
-      ggplot(aes(x = dateTime, y = Flow_Inst)) +
-      geom_line() +
-      ylab(parameterInfo$variableDescription) +
-      ggtitle(siteInfo$station_nm)
-    
+      plot_ly(x = ~dateTime, y = ~Flow_Inst, type = "scatter", name = "Discharge") |>
+#      add_lines(x = lubridate::ymd("2024-09-26"), y = range(site_data()$Flow_Inst, na.rm = TRUE),
+#                line = list(color = "red", dash = "dash"), name = "Landfall") |>
+      layout(
+        title = siteInfo$station_nm,
+        yaxis = list(title = parameterInfo$variableDescription)
+      )
   })
   
 } # SERVER
